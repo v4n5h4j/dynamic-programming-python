f1228f91860e0ec86e1b38dd100c03128f5f597c
v4n5h4j <vanshaj82@gmail.com>
2021-08-25 00:34:04 +0530

    Correct everything

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # dynamic-programming-python
 Collection of dynamic programming problems from https://youtu.be/oBt53YbR9Kk translated into Python :)
+
+## Usage
+  Open and run `main.py` by choosing the problem number (1-16). 
+  List of problems is also available in the `index` dictionary inside `main.py`.
+
+### Index
+    1_nth_fibonacci
+    2_grid_traveler
+    3_can_sum
+    4_how_sum
+    5_best_sum
+    6_can_construct
+    7_count_construct
+    8_all_construct
+    9_fib_tabulation
+    10_grid_tabulation
+    11_canSum_tabulation
+    12_howSum_tabulation
+    13_bestSum_tabulation
+    14_canConstruct_tabulation
+    15_countConstruct_tabulation
+    16_allConstruct_tabulation
+
+![main.py script running](https://i.ibb.co/yYcd2D7/yo-NOXjm-NDE.gif)
--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-import os
-import sys
-import importlib
+import os, sys, importlib
 from pathlib import Path
 sys.path.append(os.path.join(Path(__file__).parent.absolute(), 'Problems'))
-print(os.getcwd())
 
 """
 Collection of dynamic programming problems from https://youtu.be/oBt53YbR9Kk
 """
 
-index = {"1": '1_nth_fibonacci', "2": '2_grid_traveler', "3": '3_can_sum', "4": '4_how_sum', "5": '5_best_sum', "6": '6_can_construct', "7": '7_count_construct', "8": '8_all_construct', "9": '9_fib_tabulation',
-         "10": '10_grid_tabulation', "11": '11_canSum_tabulation', "12": '12_howSum_tabulation', "13": '13_bestSum_tabulation', "14": '14_canConstruct_tabulation', "15": '15_countConstruct_tabulation', "16": '16_allConstruct_tabulation'}
+index = {"1":'1_nth_fibonacci'\
+		,"2":'2_grid_traveler'\
+		,"3":'3_can_sum'\
+		,"4":'4_how_sum'\
+		,"5":'5_best_sum'\
+		,"6":'6_can_construct'\
+		,"7":'7_count_construct'\
+		,"8":'8_all_construct'\
+		,"9":'9_fib_tabulation'\
+		,"10":'10_grid_tabulation'\
+		,"11":'11_canSum_tabulation'\
+		,"12":'12_howSum_tabulation'\
+		,"13":'13_bestSum_tabulation'\
+		,"14":'14_canConstruct_tabulation'\
+		,"15":'15_countConstruct_tabulation'\
+		,"16":'16_allConstruct_tabulation'}
 
 
 while True:
